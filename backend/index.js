a67a259ef08694ac24472e7a5b21b2bd953f34a2
rvitality <vital.renzjohn@gmail.com>
2023-02-24 14:40:03 +0800

    Add endpoint to retrieve all books

--- backend/index.js
+++ backend/index.js
@@ -14,6 +14,19 @@ const db = mysql.createConnection({
 app.use(express.json());
 app.use(cors());
 
+app.get("/", (req, res) => {
+    res.json("hello this is the backend");
+});
+
+app.get("/books", (req, res) => {
+    const q = "SELECT * FROM books";
+    db.query(q, (err, data) => {
+        if (err) return res.json(err);
+
+        return res.json(data);
+    });
+});
+
 app.listen(8800, () => {
     console.log("Connected to backend!");
 });
